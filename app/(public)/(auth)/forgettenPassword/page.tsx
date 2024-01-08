1808fb878188e7c53f91e7664edfd502b4e09b27
Fode Oulare <foulare@e1r3p8.1337.ma>
2024-01-08 16:03:52 +0100

    Add a function in order to call a server action
    - add a function in order to call  a server action to send an email to
      the user that forgot his password

--- app/(public)/(auth)/forgettenPassword/page.tsx
+++ app/(public)/(auth)/forgettenPassword/page.tsx
@@ -1,11 +1,33 @@
+'use client'
+import { sendPasswordToken } from "@/actions/user/sendPasswordToken";
+import { toastOption } from "@/app/(protected)/dashboard/CreateOrganization";
 import SubmitButton from "@/components/SubmitButton";
 import React from "react";
+import { toast } from "react-toastify";
 
 const page = () => {
+
+  const action = async (e : FormData) => {
+    const response = await sendPasswordToken(e);
+    if (response.success) {
+      const { message } = response;
+      toast.success(message, toastOption);
+    } else {
+      if (typeof response.message === "string") {
+        toast.error(response.message, toastOption);
+      } else {
+        const messages: string[] = response.message;
+        messages.forEach((element) => {
+          toast.error(element, toastOption);
+        });
+      }
+    }
+  }
+
   return (
     <div className="w-full h-[100vh] flex justify-center items-center">
       <div className="w-auto h-auto bg-white py-8 px-8 shadow-lg shadow-black/30 border rounded-[10px]">
-        <form className="flex  justify-center flex-col gap-y-5 items-center">
+        <form className="flex  justify-center flex-col gap-y-5 items-center" action={action}>
           <input
             name="email"
             type="email"
